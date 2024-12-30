b000d875d10fab3b67f455a3a143c762e44ac408
Sotero Osuna <sotosugo@icloud.com>
2024-12-30 12:25:58 -0700

    Finish Chapter 3

---
+++ Chapter_03/exercise3_10.py
@@ -0,0 +1,19 @@
+# Every Function
+my_list = ['Spanish', 'English', 'German', 'French']
+print(my_list)
+print(my_list[0])
+my_list.append('Italian')
+print(my_list)
+my_list.insert(1, 'Japanese')
+print(my_list)
+del my_list[1]
+print(my_list)
+popped_element = my_list.pop()
+print(popped_element)
+my_list.remove('German')
+my_list.sort()
+print(my_list)
+print(sorted(my_list))
+my_list.reverse()
+print(my_list)
+print(len(my_list))
